f66a885a0762571f7d0273d2f4416d150aa8cf4a
root <root@nation.4zhgqyrhqwiezm1lu0leudp0ia.cx.internal.cloudapp.net>
2021-09-03 17:03:23 +0000

    Inicio

---
+++ frontend/web/src/App.js
@@ -0,0 +1,14 @@
+import { Router, Switch } from 'react-router-dom';
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import './style/css/Estilos.css';
+
+function App() {
+  return (
+          <div>
+	  </div>
+}
+
+export default App;
